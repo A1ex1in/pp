259e32eef2e837fc93cf41ad96f41fdff385092c
A1ex1in <all.ssd@mail.ru>
2023-10-07 11:15:10 +0600

    время открытия браузера уменьшено до 10 сек

--- jobVerSelen.py
+++ jobVerSelen.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 from openpyxl import Workbook
 driver = webdriver.Chrome()
 driver.get('https://iotvega.com/product')
-time.sleep(15)
+time.sleep(10)
 wb = Workbook()
 ws = wb.active
 product_elements = driver.find_elements('css selector', '.product-name')
