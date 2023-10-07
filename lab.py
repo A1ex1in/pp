8ac7677d48bb367faedcf3e416b925dd7d8de616
A1ex1in <all.ssd@mail.ru>
2023-10-07 13:04:38 +0600

    +lab

---
+++ lab.py
@@ -0,0 +1,19 @@
+import time
+from pywinauto.application import Application
+import keyboard
+app = Application().start("D:\prog\PuTTY\putty.exe -ssh linuxoid@tty.sdf.org")
+keyboard.write('Tn6CMjIoEyzng')
+keyboard.send('enter')
+time.sleep(1)
+keyboard.send('enter')
+time.sleep(1)
+keyboard.send('enter')
+time.sleep(1)
+keyboard.send('enter')
+time.sleep(1)
+keyboard.send('enter')
+time.sleep(1)
+keyboard.send('enter')
+time.sleep(1)
+keyboard.send('enter')
+time.sleep(10)
