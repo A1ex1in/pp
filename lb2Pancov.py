3cd2e649f320d3b9f47ca3e0adc2304f236e43c4
A1ex1in <all.ssd@mail.ru>
2023-10-30 18:43:17 +0600

    +red

--- lb2Pancov.py
+++ lb2Pancov.py
@@ -1,7 +1,7 @@
 import time
 from pywinauto.application import Application
 import keyboard
-app = Application().start("D:\Programs\PuTTY\putty.exe -ssh linuxoid@tty.sdf.org")
+app = Application().start("D:\prog\PuTTY\putty.exe -ssh linuxoid@tty.sdf.org")
 keyboard.write('Tn6CMjIoEyzng')
 keyboard.send('enter')
 time.sleep(1)
